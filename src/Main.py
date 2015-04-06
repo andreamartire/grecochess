00744445ecf4ea25b9edaa9b61ec55b468373c8b
andrea <andrea>
2015-04-06 21:59:29 +0200

    refactoring

--- src/Main.py
+++ src/Main.py
@@ -10,13 +10,13 @@ Created on Mar 28, 2015
 
 import Constants
 import time
-from model import QuadBitBoard
+from model import QuadBitBoard, Engine
 import Utils
 
 board = QuadBitBoard.QuadBitBoard()
 '''start = time.clock()
 for i in range(0,100000):
-    board.getAllPseudoLegalMoves(Constants.BLACK)
+    board.getAllPseudoLegalMoves(board, Constants.BLACK)
 end = time.clock()
 print end - start'''
 
@@ -24,5 +24,7 @@ bbFrom = Utils.getCellBitArrayById(0)
 bbTo = Utils.getCellBitArrayById(27)
 
 board.moveQuiet(bbFrom, bbTo)
+
+Engine.getAllPseudoLegalMoves(board, Constants.WHITE)
 
 board.showBoard(0)
